d00885d1f15b21e30a68127d3a05b1cbe1a65600
HoangPhuz <phucnh.b21cn594@stu.ptit.edu.vn>
2025-05-13 23:22:23 +0700

    Lấy danh sách món ăn lên RecyclerView & Hiển thị chi tiết món ăn

--- app/src/main/java/com/example/foodordering/Adapter/MenuAdapter.kt
+++ app/src/main/java/com/example/foodordering/Adapter/MenuAdapter.kt
@@ -2,21 +2,22 @@ package com.example.foodordering.Adapter
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.foodordering.DetailsActivity
+import com.example.foodordering.Model.MenuItem
 import com.example.foodordering.databinding.MenuItemBinding
 
 class MenuAdapter(
-    private val menuItemsName: MutableList<String>,
-    private val menuItemPrice : MutableList<String>,
-    private val menuImages:MutableList<Int>,
+    private val menuItems: List<MenuItem>,
     private val requireContext: Context)
     : RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
 
-    private val itemClickListener: OnClickListener ?= null
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
         val binding = MenuItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -27,7 +28,7 @@ class MenuAdapter(
         holder.bind(position)
     }
 
-    override fun getItemCount(): Int = menuItemsName.size
+    override fun getItemCount(): Int = menuItems.size
 
     inner class MenuViewHolder(private val binding: MenuItemBinding): RecyclerView.ViewHolder(binding.root) {
 
@@ -35,30 +36,45 @@ class MenuAdapter(
             binding.root.setOnClickListener {
                 val position = adapterPosition
                 if (position != RecyclerView.NO_POSITION) {
-                    itemClickListener?.onItemClick(position)
+                    openDetailsActivity(position)
                 }
-                //                setonclickListener to open detail
-                val intent = Intent(requireContext, DetailsActivity::class.java)
-                intent.putExtra("MenuItemName", menuItemsName[position])
-                intent.putExtra("MenuItemImage", menuImages[position])
-                requireContext.startActivity(intent)
             }
         }
 
+        private fun openDetailsActivity(position: Int) {
+            val menuItem = menuItems[position]
+            // Tạo Intent để mở DetailsActivity
+            val intent = Intent(requireContext, DetailsActivity::class.java)
+            // Truyền dữ liệu cần thiết vào Intent
+            intent.putExtra("MenuItemName", menuItem.foodName)
+            intent.putExtra("MenuItemImage", menuItem.foodImage)
+            intent.putExtra("MenuItemPrice", menuItem.foodPrice)
+            intent.putExtra("MenuItemDescription", menuItem.foodDescription)
+            intent.putExtra("MenuItemIngredients", menuItem.foodIngredient)
+
+            // Bắt đầu Activity mới
+            requireContext.startActivity(intent)
+
+        }
+
+        //Set data vào RecyclerView
         fun bind(position: Int) {
+            val menuItems = menuItems[position]
             binding.apply {
-                menuFoodName.text=menuItemsName[position]
-                menuPrice.text=menuItemPrice[position]
-                menuImage.setImageResource(menuImages[position])
+                menuFoodName.text=menuItems.foodName
+                menuPrice.text=menuItems.foodPrice
 
 
+                val Uri = Uri.parse(menuItems.foodImage)
+                Glide.with(requireContext)
+                    .load(Uri)
+                    .into(menuImage)
             }
         }
 
     }
-    interface OnClickListener {
-        fun onItemClick(position: Int)
-    }
+
+
 }
 
 
--- app/src/main/java/com/example/foodordering/MenuBottomSheetFragment.kt
+++ app/src/main/java/com/example/foodordering/MenuBottomSheetFragment.kt
@@ -1,17 +1,26 @@
 package com.example.foodordering
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodordering.Adapter.MenuAdapter
+import com.example.foodordering.Model.MenuItem
 import com.example.foodordering.databinding.FragmentMenuBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MenuBottomSheetFragment : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentMenuBottomSheetBinding
+    private lateinit var database: FirebaseDatabase
+    private lateinit var menuItems: MutableList<MenuItem>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,62 +34,51 @@ class MenuBottomSheetFragment : BottomSheetDialogFragment() {
         binding.buttonBack.setOnClickListener {
             dismiss()
         }
-        val menuFoodName = listOf(
-            "Pizza",
-            "Burger",
-            "Salad",
-            "Pizza",
-            "Burger",
-            "Salad",
-            "Pizza",
-            "Burger",
-            "Salad",
-            "Pizza",
-            "Burger",
-            "Salad"
-        )
-        val menuItemPrice = listOf(
-            "10.000đ",
-            "10.000đ",
-            "15.000đ",
-            "10.000đ",
-            "10.000đ",
-            "15.000đ",
-            "10.000đ",
-            "10.000đ",
-            "15.000đ",
-            "10.000đ",
-            "10.000đ",
-            "15.000đ"
-        )
-        val menuImage = listOf(
-            R.drawable.menu1,
-            R.drawable.menu2,
-            R.drawable.menu3,
-            R.drawable.menu1,
-            R.drawable.menu2,
-            R.drawable.menu3,
-            R.drawable.menu1,
-            R.drawable.menu2,
-            R.drawable.menu3,
-            R.drawable.menu1,
-            R.drawable.menu2,
-            R.drawable.menu3
-        )
-        val adapter = MenuAdapter(
-            ArrayList(menuFoodName),
-            ArrayList(menuItemPrice),
-            ArrayList(menuImage),
-            requireContext()
-        )
-
-        binding.menuRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        binding.menuRecyclerView.adapter = adapter
-
+        retriveMenuItems()
         return binding.root
+    }
+
+    private fun retriveMenuItems() {
+        database = FirebaseDatabase.getInstance()
+        val menuRef = database.getReference("menu")
+        menuItems = mutableListOf()
+        menuRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (menuSnapshot in snapshot.children) {
+                    val menuItem = menuSnapshot.getValue(MenuItem::class.java)
+                    menuItem?.let {
+                        menuItems.add(it)
+                    }
+                }
+                Log.d("Items", "onDataChange: data received")
+                //set adapter sau khi nhận data
+                setAdapters()
+            }
+
+
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+    private fun setAdapters() {
+        if(menuItems.isNotEmpty()){
+            val adapter = MenuAdapter(menuItems, requireContext())
+            binding.menuRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+            binding.menuRecyclerView.adapter = adapter
+            Log.d("Items", "setAdapters: data set")
+        }
+        else{
+            Log.d("Items", "setAdapters: data not set")
+        }
+
     }
 
     companion object {
 
     }
+
+
 }
