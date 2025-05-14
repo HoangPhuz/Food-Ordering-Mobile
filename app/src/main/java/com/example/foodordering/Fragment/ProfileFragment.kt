5bf5a94212c99ba0466f34c9a619186cd542ca64
Hung <hungthatsu@gmail.com>
2025-05-14 15:43:41 +0700

    xử lý cập nhật thông tin cá nhân

--- app/src/main/java/com/example/foodordering/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/foodordering/Fragment/ProfileFragment.kt
@@ -1,60 +1,91 @@
 package com.example.foodordering.Fragment
 
 import android.os.Bundle
+import android.os.UserManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.foodordering.Model.UserModel
 import com.example.foodordering.R
+import com.example.foodordering.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private lateinit var binding: FragmentProfileBinding
+
+    private val auth = FirebaseAuth.getInstance()
+    private val database = FirebaseDatabase.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.saveInforButton.setOnClickListener {
+            val name = binding.name.text.toString()
+            val address = binding.address.text.toString()
+            val phone = binding.phone.text.toString()
+            val email = binding.email.text.toString()
+            updateUserData(name, address, phone, email)
+        }
+        setUserData()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun updateUserData(name: String, address: String, phone: String, email: String) {
+        val userId = auth.currentUser?.uid
+        if(userId != null) {
+            val userRef = database.reference.child("users").child(userId)
+            val userData = hashMapOf(
+                "username" to name,
+                "address" to address,
+                "phone" to phone,
+                "email" to email
+            ).toMap()
+            userRef.setValue(userData).addOnSuccessListener {
+                Toast.makeText(requireContext(), "Cập nhật thành công", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), "Cập nhật thất bại", Toast.LENGTH_SHORT).show()
             }
+        } else {
+            Toast.makeText(requireContext(), "Vui lòng đăng nhập", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setUserData() {
+        val userId = auth.currentUser?.uid
+        if(userId != null) {
+            val userRef = database.reference.child("users").child(userId)
+            userRef.addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()) {
+                        val userProfile = snapshot.getValue(UserModel::class.java)
+                        if(userProfile != null) {
+                            binding.name.setText(userProfile.username)
+                            binding.address.setText(userProfile.address)
+                            binding.email.setText(userProfile.email)
+                            binding.phone.setText(userProfile.phone)
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+        }
+
     }
 }
--- app/src/main/java/com/example/foodordering/Model/UserModel.kt
+++ app/src/main/java/com/example/foodordering/Model/UserModel.kt
@@ -3,5 +3,7 @@ package com.example.foodordering.Model
 data class UserModel(
     val username: String?=null,
     val email: String?=null,
-    val password: String?=null
+    val password: String?=null,
+    val phone: String?=null,
+    val address: String?=null
 )
